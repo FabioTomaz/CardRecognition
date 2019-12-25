c337408bb1302a50002befff2ac237572a61fbbd
Fábio Santos <fmts@ua.pt>
2019-12-25 22:22:21 +0000

    alts

--- CoinDetector.cpp
+++ CoinDetector.cpp
@@ -7,39 +7,59 @@ using namespace std;
 float getCircularityThresh(vector<Point> cntr);
 
 
+
+Mat getSquareImage( const cv::Mat& img, int target_width = 600 )
+{
+    int width = img.cols,
+       height = img.rows;
+
+    cv::Mat square = cv::Mat::zeros( target_width, target_width, img.type() );
+
+    int max_dim = ( width >= height ) ? width : height;
+    float scale = ( ( float ) target_width ) / max_dim;
+    cv::Rect roi;
+    if ( width >= height )
+    {
+        roi.width = target_width;
+        roi.x = 0;
+        roi.height = height * scale;
+        roi.y = ( target_width - roi.height ) / 2;
+    }
+    else
+    {
+        roi.y = 0;
+        roi.height = target_width;
+        roi.width = width * scale;
+        roi.x = ( target_width - roi.width ) / 2;
+    }
+
+    cv::resize( img, square( roi ), roi.size() );
+
+    return square;
+}
+
 int main(int argc, char** argv)
 {
-	Mat img, gray;
+	Mat img, imgScaled, gray;
 	img = imread(argv[1]);
 	if (img.empty()) {
 		cout << "Wrong file or unexistant!" <<endl;
 		return -1; 
 	}
 
-	cvtColor(img, gray, CV_BGR2GRAY);
+	imgScaled = getSquareImage(img);
+
+	cvtColor(imgScaled, gray, CV_BGR2GRAY);
 
 	// smooth it, otherwise a lot of false circles may be detected
 	GaussianBlur(gray, gray, Size(3, 3), 1.5, 1.5);
 
-	namedWindow("Blurred", 0);
-	imshow("Blurred", gray);
-
-	//Calculate the Otsu threshold values
-	Mat opimg = Mat(gray.rows, gray.cols, CV_8UC1);
-	double otsu_thresh_val = threshold(
-		gray, opimg, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU
-		);
-
-	imshow("otsu", opimg);
-
-	double high_thresh_val = otsu_thresh_val,
-		lower_thresh_val = otsu_thresh_val * 0.5;
+	namedWindow("Gray Blurred Image", WINDOW_NORMAL);
+	resizeWindow("Gray Blurred Image", 600, 600);
+	imshow("Gray Blurred Image", gray);
 
 	Mat edges;
-
-	Canny(gray, edges, lower_thresh_val, high_thresh_val, 3);
-
-
+	Canny(gray, edges, 50, 200, 3);
 
 	//Morphology
 	//Dilation
@@ -54,14 +74,13 @@ int main(int argc, char** argv)
 	Canny(edges, edges, 50, 200, 3);
 
 	namedWindow("Canny", 0);
+	resizeWindow("Canny", 600, 600);
 	imshow("Canny", edges);
-
 
 	// Double check for the circles - Just the edge image at this moment produces a lot of false circles - when the Hough circles function is run
 	// Shortlisting good circle candidates by doing a contour analysis
 	vector<vector<Point>> contours, contoursfil;
 	vector<Vec4i> hierarchy;
-	Mat contourImg1 = Mat::ones(edges.rows, edges.cols, edges.type());
 	Mat contourImg2 = Mat::ones(edges.rows, edges.cols, edges.type());
 
 	//Find all contours in the edges image
@@ -72,34 +91,24 @@ int main(int argc, char** argv)
 	for (int j = 0; j < contours.size(); j++)
 	{
 		//Only give me contours that are closed (i.e. they have 0 or more children : hierarchy[j][2] >= 0) AND contours that dont have any parent (i.e. hierarchy[j][3] < 0 )
-		if ((hierarchy[j][2] >= 0) && (hierarchy[j][3] < 0))
+		if ((hierarchy[j][2] >= 0))
 		{
-			circThresh = getCircularityThresh(contours[j]);
-			// Doing a quick compactness/circularity test on the contours P^2/A for the circle the perfect is 12.56 .. we give some room as we mostly are extracting elliptical shapes also
-			if ((circThresh > 10) && (circThresh <= 30))
-			{
-				contoursfil.push_back(contours[j]);
-			}
+			contoursfil.push_back(contours[j]);
 		}
 	}
-
-
-	//	drawContours(contourImg1, contours, 0, CV_RGB(255,255,255), 1, 8,0);
-	//	imshow("Contour Image", contourImg1);
 
 	for (int j = 0; j < contoursfil.size(); j++)
 	{
 		drawContours(contourImg2, contoursfil, j, CV_RGB(255, 255, 255), 1, 8);
 	}
-	namedWindow("Contour Image Filtered", 0);
+	namedWindow("Contour Image Filtered", WINDOW_NORMAL);
+	resizeWindow("Contour Image Filtered", 600, 600);
 	imshow("Contour Image Filtered", contourImg2);
-
-
 
 	// good values for param-2 - for a image having circle contours = (75-90) ... for the edge image - (100-120)
 	vector<Vec3f> circles;
-	//HoughCircles(edges, circles, CV_HOUGH_GRADIENT, 2, gray.rows / 8, 200, 120);
-	HoughCircles(contourImg2, circles, CV_HOUGH_GRADIENT, 2, gray.rows / 8, 200, 30,15,45);
+
+	HoughCircles(contourImg2, circles, CV_HOUGH_GRADIENT, 2, gray.rows / 8, 175, 45, 30, 100);
 
 	//struct to sort the vector of pairs <int,double> based on the second double value
 	struct sort_pred {
@@ -119,41 +128,61 @@ int main(int argc, char** argv)
 		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
 		float radius = circles[i][2];
 		// draw the circle center
-		circle(img, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+		circle(imgScaled, center, 3, Scalar(0, 255, 0), -1, 8, 0);
 		// draw the circle outline
-		circle(img, center, radius, Scalar(0, 0, 255), 2, 8, 0);
-		rectangle(img, Point(center.x - radius - 5, center.y - radius - 5), Point(center.x + radius + 5, center.y + radius + 5), CV_RGB(0, 0, 255), 1, 8, 0); //Opened contour - draw a green rectangle arpund circle
+		circle(imgScaled, center, radius, Scalar(0, 0, 255), 2, 8, 0);
+		rectangle(imgScaled, Point(center.x - radius - 5, center.y - radius - 5), Point(center.x + radius + 5, center.y + radius + 5), CV_RGB(0, 0, 255), 1, 8, 0); //Opened contour - draw a green rectangle arpund circle
 		ratio = ((radius*radius) / (largestRadius*largestRadius));
-		//cout << ratio << "\n";
 
 		//Using an area ratio based discrimination .. after some trial and error with the diff sizes ... this gives good results.
-		if (ratio >= 0.85)
+		if (ratio >= 0.95)
 		{
-			putText(img, "Quarter", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
-			change = change + .25;
+			putText(imgScaled, "2 Euro", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
+			change = change + 2.0;
+		}
+		else if ((ratio >= 0.85) && (ratio<95))
+		{
+			putText(imgScaled, "1 Euro", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
+			change = change + 1.0;
 		}
 		else if ((ratio >= 0.68) && (ratio<85))
 		{
-			putText(img, "5-cents", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
-			change = change + .05;
+			putText(imgScaled, "50-cents", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
+			change = change + .50;
 		}
 		else if ((ratio >= 0.60) && (ratio<.68))
 		{
-			putText(img, "1-cent", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
-			change = change + .01;
+			putText(imgScaled, "20-cents", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
+			change = change + .20;
 		}
 		else if ((ratio >= 0.40) && (ratio<.60))
 		{
-			putText(img, "Dime", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
-			change = change + .1;
+			putText(imgScaled, "10-cents", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
+			change = change + .10;
+		}
+		else if ((ratio >= 0.20) && (ratio<.40))
+		{
+			putText(imgScaled, "5-cents", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
+			change = change + .05;
+		}
+		else if ((ratio >= 0.10) && (ratio<.20))
+		{
+			putText(imgScaled, "2-cents", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
+			change = change + .02;
+		}
+		else if ((ratio >= 0.0) && (ratio<.10))
+		{
+			putText(imgScaled, "1-cent", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
+			change = change + .01;
 		}
 
 	}
 
-	putText(img, "Total Money:" + to_string(change), Point(img.cols / 10, img.rows - img.rows / 10), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.6, CV_AA);
+	putText(imgScaled, "Total Money:" + to_string(change), Point(imgScaled.cols / 10, imgScaled.rows - imgScaled.rows / 10), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.6, CV_AA);
 
-	namedWindow("circles", 0);
-	imshow("circles", img);
+	namedWindow("circles", WINDOW_NORMAL);
+	resizeWindow("circles", 600, 600);
+	imshow("circles", imgScaled);
 
 	waitKey();
 
