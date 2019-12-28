31e733ca871e512e2d5d4acc0632cc07c0e0d6bd
Fábio Santos <fmts@ua.pt>
2019-12-28 15:51:35 +0000

    ALTS

--- CoinDetector.cpp
+++ CoinDetector.cpp
@@ -58,20 +58,25 @@ int main(int argc, char** argv)
 	resizeWindow("Gray Blurred Image", 600, 600);
 	imshow("Gray Blurred Image", gray);
 
-	Mat edges;
-	Canny(gray, edges, 50, 200, 3);
+	Mat edges, edgesOpened;
+
+	Canny(gray, edges, 50, 170, 3);
+/*
+	//threshold(gray, edges, 100, 255, CV_THRESH_OTSU);
+	adaptiveThreshold(edges, edges, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 5, 1);
 
 	//Morphology
 	//Dilation
-	int dilation_type = MORPH_RECT, dilation_size = 1;  // dilation_type = MORPH_RECT,MORPH_CROSS,MORPH_ELLIPSE
-	Mat element = getStructuringElement(dilation_type,
+	int dilation_type = MORPH_CROSS; // dilation_type = MORPH_RECT,MORPH_CROSS,MORPH_ELLIPSE
+	int dilation_size = 1;  
+	Mat element = getStructuringElement(
+		dilation_type,
 		Size(2 * dilation_size + 1, 2 * dilation_size + 1),
-		Point(dilation_size, dilation_size));
+		Point(dilation_size, dilation_size)
+	);
 
-	morphologyEx(edges, edges, MORPH_DILATE, element);
-
-	//Canny to thin the edges after dilation
-	Canny(edges, edges, 50, 200, 3);
+	morphologyEx(edges, edgesOpened, MORPH_ERODE, element);
+*/
 
 	namedWindow("Canny", 0);
 	resizeWindow("Canny", 600, 600);
@@ -81,7 +86,7 @@ int main(int argc, char** argv)
 	// Shortlisting good circle candidates by doing a contour analysis
 	vector<vector<Point>> contours, contoursfil;
 	vector<Vec4i> hierarchy;
-	Mat contourImg2 = Mat::ones(edges.rows, edges.cols, edges.type());
+/*	Mat contourImg2 = Mat::ones(edges.rows, edges.cols, edges.type());
 
 	//Find all contours in the edges image
 	findContours(edges.clone(), contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE);
@@ -91,7 +96,7 @@ int main(int argc, char** argv)
 	for (int j = 0; j < contours.size(); j++)
 	{
 		//Only give me contours that are closed (i.e. they have 0 or more children : hierarchy[j][2] >= 0) AND contours that dont have any parent (i.e. hierarchy[j][3] < 0 )
-		if ((hierarchy[j][2] >= 0))
+		if ((hierarchy[j][2] >= 0) && (hierarchy[j][3] < 0))
 		{
 			contoursfil.push_back(contours[j]);
 		}
@@ -103,12 +108,12 @@ int main(int argc, char** argv)
 	}
 	namedWindow("Contour Image Filtered", WINDOW_NORMAL);
 	resizeWindow("Contour Image Filtered", 600, 600);
-	imshow("Contour Image Filtered", contourImg2);
+	imshow("Contour Image Filtered", contourImg2);*/
 
 	// good values for param-2 - for a image having circle contours = (75-90) ... for the edge image - (100-120)
 	vector<Vec3f> circles;
 
-	HoughCircles(contourImg2, circles, CV_HOUGH_GRADIENT, 2, gray.rows / 8, 175, 45, 30, 100);
+	HoughCircles(edges, circles, CV_HOUGH_GRADIENT, 2, gray.rows / 9, 200, 100, 25, 100);
 
 	//struct to sort the vector of pairs <int,double> based on the second double value
 	struct sort_pred {
@@ -131,7 +136,15 @@ int main(int argc, char** argv)
 		circle(imgScaled, center, 3, Scalar(0, 255, 0), -1, 8, 0);
 		// draw the circle outline
 		circle(imgScaled, center, radius, Scalar(0, 0, 255), 2, 8, 0);
-		rectangle(imgScaled, Point(center.x - radius - 5, center.y - radius - 5), Point(center.x + radius + 5, center.y + radius + 5), CV_RGB(0, 0, 255), 1, 8, 0); //Opened contour - draw a green rectangle arpund circle
+		rectangle(
+			imgScaled, 
+			Point(center.x - radius - 5, center.y - radius - 5), 
+			Point(center.x + radius + 5, center.y + radius + 5), 
+			CV_RGB(0, 0, 255), 
+			1, 
+			8, 
+			0
+		); //Opened contour - draw a green rectangle arpund circle
 		ratio = ((radius*radius) / (largestRadius*largestRadius));
 
 		//Using an area ratio based discrimination .. after some trial and error with the diff sizes ... this gives good results.
@@ -140,22 +153,19 @@ int main(int argc, char** argv)
 			putText(imgScaled, "2 Euro", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
 			change = change + 2.0;
 		}
-		else if ((ratio >= 0.85) && (ratio<95))
+		else if ((ratio >= 0.70) && (ratio<95))
 		{
-			putText(imgScaled, "1 Euro", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
-			change = change + 1.0;
-		}
-		else if ((ratio >= 0.68) && (ratio<85))
-		{
-			putText(imgScaled, "50-cents", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
+			/*putText(imgScaled, "50 cents", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
+			change = change + 1.0;*/
+			putText(imgScaled, "1 euro", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
 			change = change + .50;
 		}
-		else if ((ratio >= 0.60) && (ratio<.68))
+		else if ((ratio >= 0.50) && (ratio<.70))
 		{
 			putText(imgScaled, "20-cents", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
 			change = change + .20;
 		}
-		else if ((ratio >= 0.40) && (ratio<.60))
+		else if ((ratio >= 0.40) && (ratio<.50))
 		{
 			putText(imgScaled, "10-cents", Point(center.x - radius, center.y + radius + 15), FONT_HERSHEY_COMPLEX_SMALL, 0.7, Scalar(0, 255, 255), 0.4, CV_AA);
 			change = change + .10;
@@ -199,3 +209,19 @@ float getCircularityThresh(vector<Point> cntr)
 	return ((perm*perm) / area);
 
 }
+
+/*
+approx = cv.approxPolyDP(cnt, 0.008 * cv.arcLength(cnt, True), True)
+            area = cv.contourArea(cnt)
+            if len(approx) > 12 and area > 750:
+                (x, y), radius = cv.minEnclosingCircle(cnt)
+                center = (int(x), int(y))
+                radius = int(radius)
+                if area > 8000:
+                    real_contoursB.append((center, radius))
+                    #cv.circle(original, center, radius, (255, 255, 0), 4)
+                else:
+                    real_contoursG.append((center, radius))
+                    #cv.circle(original, center, radius, (0, 255, 0), 4)
+*/
+
